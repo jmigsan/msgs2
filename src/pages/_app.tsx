abe26cba2506b96b4b56cce9fed3236f0ba3b9f0
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-01-17 16:26:26 +0000

    chakra added to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 // src/pages/_app.tsx
-import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
-import type { AppType } from "next/app";
-import { trpc } from "../utils/trpc";
+import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import type { Session } from 'next-auth';
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,7 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 };
